ebf581664ea011eccf18ae2600a1e68ce79add96
Cleany56 <54605898+Cleany56@users.noreply.github.com>
2025-08-13 21:06:39 -0400

    Github Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Portfolio/', // Replace <repository-name> with your repo name
   plugins: [react(), tailwindcss()],
 })
